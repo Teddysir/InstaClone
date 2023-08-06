cd852e391191c87d6515973749692bfb7643f8cc
강혜미 <eham1003@naver.com>
2023-08-06 20:39:40 +0900

    Feat: WebSocket을 활용한 채팅 기능 구현

---
+++ src/main/java/com/instagram/clone/socket/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.instagram.clone.socket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketChatHandler chatHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    // CORS 설정 추가
+    @Configuration
+    public static class MyConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**") // 모든 요청에 대해
+                    .allowedOrigins("*") // 모든 origin을 허용
+                    .allowedMethods("*"); // 모든 HTTP 메서드 허용
+        }
+    }
+}
